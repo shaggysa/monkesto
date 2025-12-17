e285b9f759e0f78beeadfb60290557c93516e1cb
shaggy <gabriel.couchenour@gmail.com>
2025-12-17 09:25:35 -0600

    add monkesto logo as an asset
    may need to be scaled up

--- src/site/auth.rs
+++ src/site/auth.rs
@@ -12,16 +12,7 @@ pub fn ClientLogin() -> impl IntoView {
     view! {
         <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
             <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-                <img
-                    src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                    alt="Your Company"
-                    class="mx-auto h-10 w-auto dark:hidden"
-                />
-                <img
-                    src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
-                    class="mx-auto h-10 w-auto not-dark:hidden"
-                />
+                <img src="logo.svg" alt="Monkesto" class="mx-auto h-10 w-auto" />
                 <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900 dark:text-white">
                     Sign in to your account
                 </h2>
@@ -139,16 +130,7 @@ pub fn ClientSignUp() -> impl IntoView {
     view! {
         <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
             <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-                <img
-                    src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                    alt="Your Company"
-                    class="mx-auto h-10 w-auto dark:hidden"
-                />
-                <img
-                    src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
-                    class="mx-auto h-10 w-auto not-dark:hidden"
-                />
+                <img src="logo.svg" alt="Monkesto" class="mx-auto h-10 w-auto" />
                 <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900 dark:text-white">
                     Sign Up
                 </h2>
